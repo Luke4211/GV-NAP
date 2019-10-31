7da7a712d85845763a7acce3bd9bf29f685ec9f9
Luke4211 <34666766+Luke4211@users.noreply.github.com>
2019-10-31 01:49:02 -0400

    Added retrieve functionality

---
+++ client/HostServer.java
@@ -0,0 +1,80 @@
+package client;
+
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HostServer {
+
+    public static void main(String[] args) {
+        try(ServerSocket listener = new ServerSocket(10000)) {
+            System.out.println("Server running");
+            ExecutorService threadPool = Executors.newFixedThreadPool(15);
+            while(true) {
+                threadPool.execute(new ClientHandler(listener.accept())); 
+            } 
+        } catch(Exception e){e.printStackTrace();}
+    }
+
+
+    private static class ClientHandler implements Runnable {
+        private Socket socket;
+        private InputStream serverIn;            
+        private OutputStream serverOut;
+        private String hostname;
+        private String userName;
+        private String speed;
+        
+
+
+        
+        ClientHandler(Socket socket) throws Exception {
+            this.socket = socket;
+            this.serverIn = socket.getInputStream();
+            this.serverOut = socket.getOutputStream();
+        }
+              
+        private String getMessage() throws Exception {
+        	byte[] msgLength = new byte[4];
+    		this.serverIn.read(msgLength, 0, 4);
+    		int len = ByteBuffer.wrap(msgLength).getInt();
+    		byte[] msg = new byte[len];
+    		this.serverIn.read(msg, 0, len);
+    		return new String(msg);
+        }
+        
+        void sendMessage(String send) throws Exception {
+    		byte[] msg = send.getBytes();
+    		byte[] msgLen = ByteBuffer.allocate(4).putInt(msg.length).array();		
+    		this.serverOut.write(msgLen, 0, 4);
+    		this.serverOut.write(msg, 0, msg.length);
+    	}
+
+        @Override
+        public void run() {
+        		String request;
+				try {
+					System.out.println("We are here...");
+					request = this.getMessage();
+					File send = new File(request);
+					InputStream fin = new FileInputStream(send);
+					byte[] fbytes = new byte[(int)send.length()];
+					fin.read(fbytes, 0, (int)send.length());
+					this.sendMessage(new String(fbytes));
+				} catch (Exception e) {					
+					e.printStackTrace();
+				}
+        	
+        }
+        
+    
+    }
+}
